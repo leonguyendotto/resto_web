24614864e4947a43653d231f5244f79a954ca175
leonguyendotto <leonguyen.to@MSI>
2022-11-29 14:07:15 -0500

    special menu section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -15,13 +15,13 @@ const SpecialMenu = () => (
     </div>
     <div className='app__specialMenu-menu'>
       <div className='app__specialMenu-menu_wine flex__center'>
-        <p className='app__specialMenu_menu_heading'>
+        <p className='app__specialMenu-menu_heading'>
           Wine & Beer
         </p>
         <div className='app__specialMenu_menu_items'>
           {data.wines.map((wine,index)=> (
             <MenuItem
-              key={wine.title + index} title={wine.price} price={wine.price} tags={wine.tags}
+              key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}
             />
           ))}
         </div>
@@ -32,13 +32,13 @@ const SpecialMenu = () => (
       </div>
 
       <div className='app__specialMenu-menu_cocktails flex__center'>
-        <p className='app__specialMenu_menu_heading'>
-          Wine & Beer
+        <p className='app__specialMenu-menu_heading'>
+          Cocktails
         </p>
         <div className='app__specialMenu_menu_items'>
           {data.cocktails.map((cocktail,index)=> (
             <MenuItem
-            key={cocktail.title + index} title={cocktail.price} price={cocktail.price} tags={cocktail.tags}
+            key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}
           />
           ))}
         </div>
